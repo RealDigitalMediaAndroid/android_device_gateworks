efabc65e86269ad039890ca2940da326402e5cf0
Harvey Chapman <hchapman@realdigitalmedia.com>
2015-06-11 15:30:18 -0400

    mount and update boot and recovery partitions

--- common/releasetools.py
+++ common/releasetools.py
@@ -7,6 +7,14 @@ def FullOTA_InstallEnd(info):
     for filename in fnmatch.filter(info.input_zip.namelist(), "BOOT_EXTRA/*"):
         file = info.input_zip.read(filename)
         common.ZipWriteStr(info.output_zip, filename, file)
-    info.script.AppendExtra( 'package_extract_dir("BOOT_EXTRA", "$BD1");' )
-    info.script.AppendExtra( 'package_extract_dir("BOOT_EXTRA", "$BD2");' )
+    # error: make_ext4fs_internal: cannot lookup security context for /boot/
+    # info.script.FormatPartition("/boot")
+    info.script.Mount("/boot")
+    info.script.UnpackPackageDir("BOOT_EXTRA", "/boot")
+    info.script.AppendExtra('package_extract_file("boot.img", "/boot");')
+    # TODO: do this differently, the current process generates recovery.img
+    # on the next boot. So, if boot.img is bad, we're screwed.
+    # info.script.FormatPartition("/recovery")
+    info.script.Mount("/recovery")
+    info.script.UnpackPackageDir("BOOT_EXTRA", "/recovery")
 
