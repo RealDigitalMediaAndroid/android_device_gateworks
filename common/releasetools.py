000977eb7f33bbe19aaa4ebf83b43035a481022d
Harvey Chapman <hchapman@realdigitalmedia.com>
2015-07-02 08:09:14 -0400

    add bootloader upgrade to recovery

--- common/releasetools.py
+++ common/releasetools.py
@@ -1,7 +1,7 @@
 import common
 import fnmatch
 
-def FullOTA_InstallEnd(info):
+def InstallBootAndRecovery(info):
     # Copy BOOT_EXTRA directory contents to output
     # BOOT_EXTRA/* --> [partition path]
     for filename in fnmatch.filter(info.input_zip.namelist(), "BOOT_EXTRA/*"):
@@ -18,3 +18,20 @@ def FullOTA_InstallEnd(info):
     info.script.Mount("/recovery")
     info.script.UnpackPackageDir("BOOT_EXTRA", "/recovery")
 
+def InstallBootloader(info):
+    # Copy BOOTLOADER directory contents to output
+    # BOOTLOADER/* --> [partition path]
+    for filename in fnmatch.filter(info.input_zip.namelist(), "BOOTLOADER/*"):
+        file = info.input_zip.read(filename)
+        common.ZipWriteStr(info.output_zip, filename, file)
+    # Install SPL, u-boot.img, and erase u-boot environment
+    info.script.AppendExtra('package_extract_file("BOOTLOADER/SPL", "/SPL");')
+    info.script.AppendExtra('package_extract_file("BOOTLOADER/u-boot.img", "/u-boot.img");')
+    info.script.AppendExtra('run_program("/res/kobs-ng", "init", "-v", "-x", "--search_exponent=1", "--chip_0_size=0xe00000", "--chip_0_device_path=/dev/mtd/mtd0", "/SPL");')
+    info.script.AppendExtra('run_program("/res/flash_erase", "/dev/mtd/mtd0", "0xe00000", "0");')
+    info.script.AppendExtra('run_program("/res/nandwrite", "--start=0xe00000", "--pad", "/dev/mtd/mtd0", "/u-boot.img");')
+    info.script.AppendExtra('run_program("/res/flash_erase", "/dev/mtd/mtd1", "0", "0");')
+
+def FullOTA_InstallEnd(info):
+    InstallBootAndRecovery(info)
+    InstallBootloader(info)
